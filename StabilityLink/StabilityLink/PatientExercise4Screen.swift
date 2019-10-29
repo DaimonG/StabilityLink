d44c74a5889f006c0b2e0f3c3c903070479423a5
Daimon Gill <daimongill6@gmail.com>
2019-10-28 19:43:25 -0700

    Added view controller Swift files for each scene

---
+++ StabilityLink/StabilityLink/PatientExercise4Screen.swift
@@ -0,0 +1,30 @@
+//
+//  PatientExercise4Screen.swift
+//  StabilityLink
+//
+//  Created by Daimon Gill on 2019-10-28.
+//  Copyright © 2019 Daimon Gill. All rights reserved.
+//
+
+import UIKit
+
+class PatientExercise4Screen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
