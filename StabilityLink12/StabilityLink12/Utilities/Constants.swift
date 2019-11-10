3024a76a949ab9cbc0469d603a2d1b2dba41c703
Alex Makasoff <alexmakasoff@gmail.com>
2019-11-10 15:53:19 -0800

    First commit for new project layout

---
+++ StabilityLink12/StabilityLink12/Utilities/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  StabilityLink12
+//
+//  Created by Alex Makasoff on 2019-11-10.
+//  Copyright © 2019 Matthew Chute. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let slDarkGrey = UIColor(red: 60/255, green: 60/255, blue: 60/255, alpha: 1)
+}
+
+struct Fonts {
+    static let avenirNextMedium = "AvenirNext-Medium"
+}
