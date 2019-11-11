9c60b8cbaf19c0c070a56cb80bb0bdca50968d67
Alex Makasoff <alexmakasoff@gmail.com>
2019-11-11 11:02:27 -0800

    Finished patient home migration

---
+++ StabilityLink12/StabilityLink12/Screens/PatientRoutineTableScreen.swift
@@ -0,0 +1,21 @@
+//
+//  PatientRoutineTableScreen.swift
+//  StabilityLink12
+//
+//  Created by Alex Makasoff on 2019-11-11.
+//  Copyright © 2019 Matthew Chute. All rights reserved.
+//
+
+import UIKit
+
+class PatientRoutineTableScreen: UIViewController {
+    
+    // TODO: Create a routine class to store in the array of routines
+    var routines: [Int] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
