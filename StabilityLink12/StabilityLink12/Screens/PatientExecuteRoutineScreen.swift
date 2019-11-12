5e7c5f0fbabe265c9dcb9194331c98716a3aac21
Alex Makasoff <alexmakasoff@gmail.com>
2019-11-11 16:46:04 -0800

    added patient execute routine swift file

---
+++ StabilityLink12/StabilityLink12/Screens/PatientExecuteRoutineScreen.swift
@@ -0,0 +1,30 @@
+//
+//  PatientExecuteRoutineScreen.swift
+//  StabilityLink12
+//
+//  Created by Alex Makasoff on 2019-11-11.
+//  Copyright © 2019 Matthew Chute. All rights reserved.
+//
+
+import UIKit
+
+class PatientExecuteRoutineScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
