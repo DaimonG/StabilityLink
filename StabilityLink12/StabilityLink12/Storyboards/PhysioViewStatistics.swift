70790f6932286662484b5adc607a81685c75778e
luicurtis <lui_curtis@hotmail.com>
2019-11-30 17:42:53 -0800

    Physio stats working with x axis labels

--- StabilityLink12/StabilityLink12/Storyboards/PhysioViewStatistics.swift
+++ StabilityLink12/StabilityLink12/Storyboards/PhysioViewStatistics.swift
@@ -23,13 +23,14 @@ class PhysioViewStatistics: UIViewController {
     var exerName = ""
     var TimesCount = ""
     weak var axisFormatDelegate: IAxisValueFormatter?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // Pull ExerciseData using nested loop
         self.ref = Database.database().reference()
+        
         self.ref?.child("users").child(currentPatient).child("routines").child(selectedRoutine).observeSingleEvent(of: DataEventType .value, with: { (snapshot) in
             for routine in snapshot.children.allObjects as![DataSnapshot]{
                 let snap = routine.value as? [String : String]
@@ -41,50 +42,41 @@ class PhysioViewStatistics: UIViewController {
                 self.yValue.append(self.TimesCount)
                 
             }
+            
+            // Cast all values of string into double
+            self.yValue_Double = self.yValue.compactMap { (value) -> Double? in
+                return Double(value)
+            }
+        
+            // Update the chart
+            self.setChart(dataEntryX: self.xValue, dataEntryY: self.yValue_Double )
         })
-        
-        // Cast all values of string into double
-        let yValue_Double = yValue.map { (value) -> Double? in
-            return Double(value)
-        }
-        
-        // Update the chart
-        setChart(dataEntryX: xValue, dataEntryY: yValue_Double as! [Double])
     }
     
     func setChart(dataEntryX forX:[String],dataEntryY forY: [Double]) {
         barChart.noDataText = "You need to provide data for the chart."
+        
         var dataEntries:[BarChartDataEntry] = []
+        
+        // Enter data as pairs into dataEntries
         for i in 0..<forX.count{
-           // print(forX[i])
-           // let dataEntry = BarChartDataEntry(x: (forX[i] as NSString).doubleValue, y: Double(unitsSold[i]))
             let dataEntry = BarChartDataEntry(x: Double(i), y: Double(forY[i]) , data: xValue as AnyObject?)
             print(dataEntry)
             dataEntries.append(dataEntry)
         }
+        
         let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Number of Completions")
         let chartData = BarChartData(dataSet: chartDataSet)
         barChart.data = chartData
-        let xAxisValue = barChart.xAxis
-        xAxisValue.valueFormatter = axisFormatDelegate
+
+        barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: xValue)
+        barChart.xAxis.granularity = 1
 
         }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
-extension PhysioViewStatistics: IAxisValueFormatter {
-
-    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
-        return xValue[Int(value)]
-    }
-}
+//extension PhysioViewStatistics: IAxisValueFormatter {
+//
+//    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+//    return xValue[Int(value)]
+//    }
+//}
