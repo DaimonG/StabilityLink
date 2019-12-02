40e6c0e635decfade7c0fd644c959c0e484682c4
matthewchute <mchute@sfu.ca>
2019-12-01 18:11:43 -0800

    camera is working

--- StabilityLink12/StabilityLink12/Storyboards/RecordVideoViewController.swift
+++ StabilityLink12/StabilityLink12/Storyboards/RecordVideoViewController.swift
@@ -28,14 +28,50 @@
 
 import UIKit
 import MobileCoreServices
+import AVKit
 
-class RecordVideoViewController: UIViewController {
+class RecordVideoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,UITextFieldDelegate,UICollectionViewDelegateFlowLayout {
   
-  @IBAction func record(_ sender: AnyObject) {
-    VideoHelper.startMediaBrowser(delegate: self, sourceType: .camera)
     
+    
+ var videoAndImageReview = UIImagePickerController()
+ var videoURL : URL?
+  @IBAction func record(_ sender: AnyObject) {
+    let mediaUI = UIImagePickerController()
+    mediaUI.mediaTypes = [kUTTypeMovie as String]
+    mediaUI.sourceType = .camera
+    mediaUI.videoQuality = .typeHigh
+    mediaUI.allowsEditing = true
+    mediaUI.delegate = self
+    self.present(mediaUI, animated: true, completion: nil)
+    /*
+    VideoHelper.startMediaBrowser(delegate: self, sourceType: .camera)
+    */
     }
     
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      dismiss(animated: true, completion: nil)
+      
+      guard let mediaType = info[UIImagePickerController.InfoKey.mediaType] as? String,
+      mediaType == (kUTTypeMovie as String),
+      let url = info[UIImagePickerController.InfoKey.mediaURL] as? URL,
+      UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url.path)
+      else { return }
+      
+      // Handle a movie capture
+          
+      UISaveVideoAtPathToSavedPhotosAlbum(url.path, self, #selector(video(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
+       let title = (error == nil) ? "Success" : "Error"
+       let message = (error == nil) ? "Video was saved" : "Video failed to save"
+       
+       let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+       alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil))
+       present(alert, animated: true, completion: nil)
+    }
+/*
   @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
     let title = (error == nil) ? "Success" : "Error"
     let message = (error == nil) ? "Video was saved" : "Video failed to save"
@@ -43,12 +79,12 @@ class RecordVideoViewController: UIViewController {
     let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
     alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil))
     present(alert, animated: true, completion: nil)
-  }
+  }*/
   
 }
 
 // MARK: - UIImagePickerControllerDelegate
-
+/*
 extension RecordVideoViewController: UIImagePickerControllerDelegate {
     private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
     dismiss(animated: true, completion: nil)
@@ -70,3 +106,4 @@ extension RecordVideoViewController: UIImagePickerControllerDelegate {
 
 extension RecordVideoViewController: UINavigationControllerDelegate {
 }
+*/
